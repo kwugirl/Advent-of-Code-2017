5e4402adf344bbd4e3b80764e3f1a7a9702cbfd6
Katherine Wu <kwugirl@gmail.com>
2018-01-02 18:02:58 -0800

    Day 1a: solve captcha

--- day_1_tests.rb
+++ day_1_tests.rb
@@ -3,12 +3,12 @@ require './day_1'
 
 class Day1Test < Minitest::Test
 
-  # def test_given_examples
-  #   assert_equal 3, solve_captcha("1122")
-  #   assert_equal 4, solve_captcha("1111")
-  #   assert_equal 0, solve_captcha("1234")
-  #   assert_equal 9, solve_captcha("91212129")
-  # end
+  def test_given_examples
+    assert_equal 3, solve("1122")
+    assert_equal 4, solve("1111")
+    assert_equal 0, solve("1234")
+    assert_equal 9, solve("91212129")
+  end
 
   def test_process_input
     assert_equal [1,1,2,2,1], process("1122")
