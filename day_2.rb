fc07e14aadb929b6a06afd33aaf35ca7e757b018
Katherine Wu <kwugirl@gmail.com>
2018-01-02 20:14:14 -0800

    Day 2a: calculate checksum for file

--- day_2.rb
+++ day_2.rb
@@ -2,3 +2,13 @@ def calculate_row_checksum row
   processed_input = row.split(" ").map(&:to_i).sort
   processed_input[-1] - processed_input[0]
 end
+
+def calculate_file_checksum file
+  sum = 0
+  File.readlines(file).each do |line|
+    sum += calculate_row_checksum(line)
+  end
+  sum
+end
+
+puts calculate_file_checksum("day_2_input.txt")
--- day_2_tests.rb
+++ day_2_tests.rb
@@ -8,4 +8,8 @@ class Day2Test < Minitest::Test
     assert_equal 4, calculate_row_checksum("7 5 3")
     assert_equal 6, calculate_row_checksum("2 4 6 8")
   end
+
+  def test_calculate_file_checksum
+    assert_equal 18, calculate_file_checksum("day_2_test_input.txt")
+  end
 end
